7e61a84ef51a38d5efba2040ff8a39a39c3ce22a
Inifynastic <1abiral6190@gmail.com>
2024-08-19 20:01:52 +0545

    Added the game file

---
+++ maybeinfuture.cpp
@@ -0,0 +1,71 @@
+    /*
+    int  spaces2[9]{' ',' ',' ',' ',' ',' ',' ',' ',' '};
+    for(int i = 0; i < 9; ++i){
+        if(spaces[i] == 'O'){
+            spaces2[i] == 0;
+        }else if(spaces[i] == 'X'){
+            spaces2[i] == 1; 
+        }
+    }
+        break;
+    
+    default:
+        break;
+    }
+
+    return 0;
+*/
+
+/*
+     if(spaces[0] != ' ' && spaces[0] == spaces[1] == spaces [2]){
+        spaces[0] == player1 ? std::cout<<"Player 1 Wins!("<< player1<<")"<<std::endl : 
+        std::cout<<"Player 2 Wins!("<< player2<<")"<<std::endl;
+    }else if(spaces[3] != ' ' && spaces[3] == spaces[4] == spaces [5]){
+        spaces[3] == player1 ? std::cout<<"Player 1 Wins!("<< player1<<")"<<std::endl : 
+        std::cout<<"Player 2 Wins!("<< player2<<")"<<std::endl;
+    }else if(spaces[6] != ' ' && spaces[6] == spaces[7] == spaces [8]){
+        spaces[6] == player1 ? std::cout<<"Player 1 Wins!("<< player1<<")"<<std::endl : 
+        std::cout<<"Player 2 Wins!("<< player2<<")"<<std::endl;
+    }else if(spaces[0] != ' ' && spaces[0] == spaces[3] == spaces [6]){
+        spaces[0] == player1 ? std::cout<<"Player 1 Wins!("<< player1<<")"<<std::endl : 
+        std::cout<<"Player 2 Wins!("<< player2<<")"<<std::endl;
+    }else if(spaces[1] != ' ' && spaces[1] == spaces[4] == spaces [7]){
+        spaces[1] == player1 ? std::cout<<"Player 1 Wins!("<< player1<<")"<<std::endl : 
+        std::cout<<"Player 2 Wins!("<< player2<<")"<<std::endl;
+    }else if(spaces[2] != ' ' && spaces[2] == spaces[5] == spaces [8]){
+        spaces[2] == player1 ? std::cout<<"Player 1 Wins!("<< player1<<")"<<std::endl : 
+        std::cout<<"Player 2 Wins!("<< player2<<")"<<std::endl;
+    }else if(spaces[6] != ' ' && spaces[6] == spaces[4] == spaces [2]){
+        spaces[6] == player1 ? std::cout<<"Player 1 Wins!("<< player1<<")"<<std::endl : 
+        std::cout<<"Player 2 Wins!("<< player2<<")"<<std::endl;
+    }else if(spaces[0] != ' ' && spaces[0] == spaces[4] == spaces [8]){
+        spaces[0] == player1 ? std::cout<<"Player 1 Wins!("<< player1<<")"<<std::endl : 
+        std::cout<<"Player 2 Wins!("<< player2<<")"<<std::endl;
+    }else{
+        return false;
+    }
+    return true;
+    */
+
+   /*
+       if(spaces[0] = ' ' && spaces[0] == spaces[1] == spaces[2]){
+        spaces[0] == player1 ? std::cout<<"Player wins \n" : std::cout<<"Computer Wins \n";
+    }else if(spaces[3] = ' ' && spaces[3] == spaces[4] == spaces[5]){
+        spaces[3] == player1 ? std::cout<<"Player wins \n" : std::cout<<"Computer Wins \n";
+    }else if(spaces[6] = ' ' && spaces[6] == spaces[7] == spaces[8]){
+        spaces[6] == player1 ? std::cout<<"Player wins \n" : std::cout<<"Computer Wins \n";
+    }else if(spaces[0] = ' ' && spaces[0] == spaces[3] == spaces[6]){
+        spaces[0] == player1 ? std::cout<<"Player wins \n" : std::cout<<"Computer Wins \n";
+    }else if(spaces[1] = ' ' && spaces[1] == spaces[4] == spaces[7]){
+        spaces[1] == player1 ? std::cout<<"Player wins \n" : std::cout<<"Computer Wins \n";
+    }else if(spaces[2] = ' ' && spaces[2] == spaces[5] == spaces[8]){
+        spaces[2] == player1 ? std::cout<<"Player wins \n" : std::cout<<"Computer Wins \n";
+    }else if(spaces[6] = ' ' && spaces[6] == spaces[4] == spaces[2]){
+        spaces[6] == player1 ? std::cout<<"Player wins \n" : std::cout<<"Computer Wins \n";
+    }else if(spaces[0] = ' ' && spaces[0] == spaces[4] == spaces[8]){
+        spaces[0] == player1 ? std::cout<<"Player wins \n" : std::cout<<"Computer Wins \n";
+    }else{
+        return false;
+    }
+    return true;
+    */
